9555e160df2949048277ec838034aa86149b022d
AnimeArcadia <animearcadia27@gmail.com>
2025-08-23 23:02:55 +0530

    aaa

---
+++ api/sections.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+include '_inc/confic.php';
+
+$db = json_decode(file_get_contents($database_path . '/sections.json'), true);
+if (json_last_error() === JSON_ERROR_NONE) {
+    echo json_encode($db);
+} else {
+    echo json_encode([]);
+}
