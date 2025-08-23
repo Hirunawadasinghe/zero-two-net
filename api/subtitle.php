67ade0eed3fcf2fde9257a872a7de13f621c605d
AnimeArcadia <animearcadia27@gmail.com>
2025-08-23 18:31:07 +0530

    aaa

---
+++ api/subtitle.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+include '_inc/confic.php';
+
+$d = json_decode(file_get_contents($database_path . '/subtitle.json'), true);
+if (json_last_error() === JSON_ERROR_NONE) {
+    echo json_encode($subtitle_data);
+} else {
+    echo json_encode([]);
+}
