c54e44391d60aaf49a55fe9c64fde9728ed15fca
AnimeArcadia <animearcadia27@gmail.com>
2025-08-23 17:06:13 +0530

    aaa

--- api/source.php
+++ api/source.php
@@ -1,2 +1,19 @@
 <?Php
-echo 'hi';
+include '_inc/confic.php';
+
+$source_data = [];
+$parts = [
+    '/vid-src/bjax0.json',
+    '/vid-src/bjax1.json',
+    '/vid-src/bjax2.json',
+    '/vid-src/bjax3.json',
+    '/vid-src/bjax4.json'
+];
+foreach ($parts as $p) {
+    $d = json_decode(file_get_contents($database_path . $p), true);
+    if (json_last_error() === JSON_ERROR_NONE) {
+        $source_data = array_merge($source_data, $d);
+    }
+}
+
+echo json_encode($source_data);
