78ef84ca71f23a863097f3d39980a4f3da2074f7
AnimeArcadia <animearcadia27@gmail.com>
2025-08-24 14:55:58 +0530

    aaa

--- api/subtitle.php
+++ api/subtitle.php
@@ -3,8 +3,16 @@ header('Content-Type: application/json');
 include '_inc/confic.php';
 
 $db = json_decode(file_get_contents($database_path . '/subtitle.json'), true);
-if (json_last_error() === JSON_ERROR_NONE) {
-    echo json_encode($db);
-} else {
-    echo json_encode([]);
+if (json_last_error() !== JSON_ERROR_NONE) {
+    die(json_encode([]));
+} else if (empty($_GET['id'])) {
+    die(json_encode($db));
+}
+
+$files = scandir($_SERVER['DOCUMENT_ROOT'] . '/subtitle');
+
+foreach ($files as $file) {
+    if ($file !== '.' && $file !== '..') { // skip current and parent directory
+        $filesArray[] = $file;
+    }
 }
