f0fd2a760f10660967c10bba65d439579f767e0f
Hiruna Wadasinghe <84002252+Hirunawadasinghe@users.noreply.github.com>
2025-08-30 13:14:12 +0530

    Update main.php

--- api/main.php
+++ api/main.php
@@ -3,16 +3,23 @@ header('Content-Type: application/json');
 include $_SERVER['DOCUMENT_ROOT'] . '/_inc/config.php';
 
 $parts = [
-    '/main/ajax1.json',
     '/main/ajax0.json',
+    '/main/ajax1.json',
     '/main/ajax2.json'
 ];
-$main_data = [];
+$db = [];
 foreach ($parts as $p) {
     $d = json_decode(file_get_contents($database_path . $p), true);
     if (json_last_error() === JSON_ERROR_NONE) {
-        $main_data = array_merge($main_data, $d);
+        $db = array_merge($db, $d);
     }
 }
 
-echo json_encode($main_data);
+$page = empty($_GET['page']) ? 1 : max($_GET['page'], 1);
+$r = [];
+for ($i = $page * $max_elements - $max_elements; $i < $max_elements * $page && $i < count($db); $i++) {
+    $r['data'][] = $db[$i];
+}
+$r['next_page'] = $page < count($db) / $max_elements ? $page + 1 : false;
+
+echo json_encode($r);
