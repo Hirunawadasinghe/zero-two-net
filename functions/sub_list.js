8e74dd82b331438c5e27c9d45fbb6a85ec26a22f
Hiruna Wadasinghe <84002252+Hirunawadasinghe@users.noreply.github.com>
2025-10-12 14:40:45 +0530

    Delete functions directory

--- functions/sub_list.js
+++
@@ -1,8 +0,0 @@
-import { readdirSync } from "fs";
-
-export async function onRequest(context) {
-  const files = readdirSync("./subtitle");
-  return new Response(JSON.stringify(files), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
