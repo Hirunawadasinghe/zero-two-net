51f44e15c3490d163c10a9e6bc4eb3627be8652f
Hiruna Wadasinghe <84002252+Hirunawadasinghe@users.noreply.github.com>
2025-10-26 00:18:53 +0530

    Update subtitle.php

--- api/subtitle.php
+++ api/subtitle.php
@@ -5,18 +5,18 @@ include $_SERVER['DOCUMENT_ROOT'] . '/_inc/b2-function.php';
 $selected_id = empty($_GET['id']) ? null : $_GET['id'];
 $subtitle_data = json_decode(file_get_contents($database_path . '/subtitle.json'), true);
 if (json_last_error() !== JSON_ERROR_NONE) {
-    die(json_encode([]));
+    die(json_encode(['status' => false, 'msg' => 'error loading database']));
 } else if (!$selected_id) {
-    die(json_encode(['data' => $subtitle_data]));
+    die(json_encode(['status' => true, 'data' => $subtitle_data]));
 }
 
 // AUTH & BUCKET
 $auth = b2_authorize();
 if (!$auth)
-    die(json_encode(['error' => 'B2 auth failed']));
+    die(json_encode(['status' => false, 'msg' => 'b2 auth failed']));
 $bucket_id = b2_get_bucket_id($auth['apiUrl'], $auth['authorizationToken']);
 if (!$bucket_id)
-    die(json_encode(['error' => 'Bucket not found']));
+    die(json_encode(['status' => false, 'msg' => 'bucket not found']));
 
 $download_url = $auth['downloadUrl'];
 $auth_token = $auth['authorizationToken'];
@@ -60,11 +60,11 @@ foreach ($subtitle_data['subtitles'] as $e) {
                 'lan_code' => $sub['lan'],
                 'language' => $subtitle_data['iso_codes'][$sub['lan']] ?? $sub['lan'],
                 'folder' => $sub['url'],
-                'base' => '//' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/sub-dl.php?f=' . $lang_folder,
+                'base' => '//' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/sub-dl?f=' . $lang_folder,
                 'data' => $file_d
             ];
         }
     }
 }
 
-echo json_encode(['data' => $sub_data]);
+echo json_encode(['status' => true, 'data' => $sub_data]);
