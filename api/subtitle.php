3465b6ebb3e566033a85a5144dab3c644c4d9b80
Hiruna Wadasinghe <84002252+Hirunawadasinghe@users.noreply.github.com>
2025-11-01 14:26:35 +0530

    Update subtitle.php

--- api/subtitle.php
+++ api/subtitle.php
@@ -38,7 +38,13 @@ foreach ($subtitle_data['subtitles'] as $e) {
 
     foreach ($e['sub'] as $sub) {
         $folder_path = strtolower($subtitle_data['iso_codes'][$sub['lan']]) . '/' . $sub['url'];
-        $files = b2_list_files($auth['apiUrl'], $auth_token, $bucket_id, $folder_path);
+
+        $files;
+        $try = 0;
+        while ($try < 2 && !$files) {
+            $files = b2_list_files($auth['apiUrl'], $auth_token, $bucket_id, $folder_path);
+            $try++;
+        }
 
         $file_d = [];
         foreach ($files as $file) {
