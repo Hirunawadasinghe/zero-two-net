48a5143b7160d2af6bc1f46512384cf072b89fdb
Hiruna Wadasinghe <84002252+Hirunawadasinghe@users.noreply.github.com>
2025-11-01 17:11:58 +0530

    Update subtitle.php

--- api/subtitle.php
+++ api/subtitle.php
@@ -55,7 +55,7 @@ foreach ($subtitle_data['subtitles'] as $e) {
                 'lan_code' => $sub['lan'],
                 'language' => $subtitle_data['iso_codes'][$sub['lan']] ?? $sub['lan'],
                 'folder' => $sub['url'],
-                'base' => dirname(((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']) . '/sub-dl.php?f=' . $folder_path,
+                'base' => dirname('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']) . '/sub-dl?f=' . $folder_path,
                 'data' => $file_d
             ];
         }
