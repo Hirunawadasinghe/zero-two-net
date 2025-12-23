edb3c1f6ea66c2e96c792079557d891ea057d4bd
Hiruna Wadasinghe <84002252+Hirunawadasinghe@users.noreply.github.com>
2025-12-23 17:01:54 +0530

    Refactor file download response to JSON format
    Return JSON response with file data or error message.

--- api/sub-dl.php
+++ api/sub-dl.php
@@ -2,26 +2,21 @@
 header('Content-Type: application/json');
 
 $file_path = $_GET['f'] ?? null;
-if (!$file_path) {
+if (!$file_path)
     die(json_encode(['status' => false, 'msg' => 'file not specified']));
-}
 
 include dirname(__DIR__, 1) . '/_inc/b2-function.php';
 
 $auth = b2_authorize();
-if (!$auth) {
+if (!$auth)
     die(json_encode(['status' => false, 'msg' => 'B2 authorization failed']));
-}
 
 $auth_token = $auth['authorizationToken'];
 $download_url = $auth['downloadUrl'];
 $fileName = $B2_BUCKET_NAME . '/' . $file_path;
 
 $file_data = b2_download_file($download_url, $auth_token, $fileName, 3);
-if ($file_data === false) {
+if ($file_data === false)
     die(json_encode(['status' => false, 'msg' => 'failed to retrieve file']));
-}
 
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="' . basename($file_path) . '"');
-echo $file_data;
+echo json_encode(['status' => true, 'data' => $file_data], JSON_UNESCAPED_UNICODE);
